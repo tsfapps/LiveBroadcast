63576ed0527f7de912b51fddcea26d89f82d7c1c
AppsLeloTsf <appslelo.com@gmail.com>
2018-12-06 15:04:42 +0530

    Bug Fixed

--- app/src/main/java/com/livebroadcast/livebroadcast/MainActivity.java
+++ app/src/main/java/com/livebroadcast/livebroadcast/MainActivity.java
@@ -2,18 +2,29 @@ package com.livebroadcast.livebroadcast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
-    private TextView textView;
-    private Button button;
+    @BindView(R.id.et_user_name)
+    EditText et_name;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_reg)
+    public void onRegister(View view){
+
+
     }
 }
