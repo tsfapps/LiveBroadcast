a103405c10c5e8a3aae6b81b7a8ba0731f8ac091
HypemaxPC-11 <tousifranchwi@gmail.com>
2018-12-06 13:39:24 +0530

    Bug Fixed

--- app/src/main/java/com/livebroadcast/livebroadcast/MainActivity.java
+++ app/src/main/java/com/livebroadcast/livebroadcast/MainActivity.java
@@ -2,6 +2,9 @@ package com.livebroadcast.livebroadcast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
